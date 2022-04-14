4834fa04c92cbcda8b663b1dfa22f433056af6cd
chuma6619 <57876553+chuma6619@users.noreply.github.com>
2022-04-14 22:01:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Redirectpage from './Components/Redirectpage';
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''>
         <div className="App">
           <Routes>
           <Route path="/" element={<Form />} />
